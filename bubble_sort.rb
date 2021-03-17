d732f70cfb3f0b9dd6ebbe9e9541da003877ceb9
Zil Norvilis <zilasino27@gmail.com>
2021-03-17 10:08:14 +0100

    Add buble_sort_by function

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,22 +1,44 @@
-def bubble_sort(arr)
+# def bubble_sort(arr)
+#   sorted = false
+#   if !sorted 
+#     sorted=true
+#     arr.each_with_index do |item, index|
+#       if arr[index+1] &&  item > arr[index + 1]
+#         arr[index]=arr[index + 1]
+#         arr[index+1]=item
+#         sorted = false
+#       end
+#     end
+#   end
+#   if sorted == true
+#     arr
+#   else
+#     bubble_sort(arr)
+#   end
+# end
+
+# p bubble_sort([4,3,78,2,0,2])
+
+
+
+def bubble_sort_by(arr)
   sorted = false
-  if !sorted 
-    sorted=true
+  until sorted
+    sorted = true
     arr.each_with_index do |item, index|
-      if arr[index+1]
-        if item > arr[index + 1]
-          arr[index]=arr[index + 1]
-          arr[index+1]=item
-        end
-      end
+      next unless arr[index + 1] && (yield(item, arr[index + 1]) > 0)
+
+      arr[index] = arr[index + 1]
+      arr[index + 1] = item
       sorted = false
     end
   end
+
   if sorted == true
-    arr 
-  else 
-    bubble_sort(arr)
+    arr
+  else
+    bubble_sort_by(arr)
   end
 end
 
-p bubble_sort([4,3,78,2,0,2])
+p bubble_sort_by(['sabaka', 'hi', 'hello', 'hey']) { |left, right| left.length - right.length }
