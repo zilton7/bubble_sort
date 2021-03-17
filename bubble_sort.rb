5d0afbf0082a92e96c65ea1ca2b0048b6faeb1c4
Zil Norvilis <zilasino27@gmail.com>
2021-03-17 10:43:35 +0100

    Fix linter issues

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,32 +1,30 @@
-# def bubble_sort(arr)
-#   sorted = false
-#   if !sorted 
-#     sorted=true
-#     arr.each_with_index do |item, index|
-#       if arr[index+1] &&  item > arr[index + 1]
-#         arr[index]=arr[index + 1]
-#         arr[index+1]=item
-#         sorted = false
-#       end
-#     end
-#   end
-#   if sorted == true
-#     arr
-#   else
-#     bubble_sort(arr)
-#   end
-# end
+def bubble_sort(arr)
+  sorted = false
+  unless sorted
+    sorted = true
+    arr.each_with_index do |item, index|
+      next unless arr[index + 1] && item > arr[index + 1]
+
+      arr[index] = arr[index + 1]
+      arr[index + 1] = item
+      sorted = false
+    end
+  end
+  if sorted == true
+    arr
+  else
+    bubble_sort(arr)
+  end
+end
 
 # p bubble_sort([4,3,78,2,0,2])
-
-
 
 def bubble_sort_by(arr)
   sorted = false
   until sorted
     sorted = true
     arr.each_with_index do |item, index|
-      next unless arr[index + 1] && (yield(item, arr[index + 1]) > 0)
+      next unless arr[index + 1] && yield(item, arr[index + 1]).positive?
 
       arr[index] = arr[index + 1]
       arr[index + 1] = item
@@ -41,4 +39,4 @@ def bubble_sort_by(arr)
   end
 end
 
-p bubble_sort_by(['sabaka', 'hi', 'hello', 'hey']) { |left, right| left.length - right.length }
+# p bubble_sort_by(['sabaka', 'hi', 'hello', 'hey']) { |left, right| left.length - right.length }
